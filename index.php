fb8281ec88b5af7ee8437ca7dbd3642b00a02c0e
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-10-04 14:19:45 +0500

    we learn variables

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
 <?php
 
+
+// Print Functions;
+
+//Difference in echo an print;
+
+//Echo Get Multiple Values;
 echo"Hello world","Hi Ameer Ali";
+
+//Print only get on value;
 print("hello");
+
+// Php buildin Function for multiple values like complete html page;
+//Starting point;
+echo<<<my
+<h1>Hello World</h1>
+<br>
+<h2>Hi Ameer ali</h2>
+
+my;
+//Ending point
+
+//Variables
+$num=3.4;
+echo "Is num : ",$num;
+
+//const variable first name is const variable name and after values;
+define("a","Hello const variable");
+
+
+//Dump use for value check any argument;
+var_dump($num)
