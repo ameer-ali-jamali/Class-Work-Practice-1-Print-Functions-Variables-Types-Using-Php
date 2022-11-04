7f5cac3a8cda7796a2322261e4ad483c8cb6e26b
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-11-04 14:20:45 +0500

    clear some mistakes

--- index.php
+++ index.php
@@ -1,4 +1,37 @@
 <?php
+// This is a single-line comment
+# This is also a single-line comment
+?>
+
+<?php
+/*
+This is a
+multiple line
+Comment.
+*/
+?>
+
+<?php
+// PHP code goes here
+?>
+
+<?php
+
+$txt = "Hello world!";  # Type String
+$x = 5;                        # Type int
+$y = 10.5;         # Type Float
+
+//Variable
+$num = 3.4;
+echo "Is num : ", $num;
+
+//const variable first name is const variable name and after values;
+define("a", "Hello const variable");
+
+//....
+
+//Dump use for value check any argument;
+var_dump($num);
 
 
 // Print Functions;
@@ -6,14 +39,14 @@
 //Difference in echo an print;
 
 //Echo Get Multiple Values;
-echo"Hello world","Hi Ameer Ali";
+echo "Hello world", "Hi Ameer Ali";
 
 //Print only get on value;
 print("hello");
 
 // Php buildin Function for multiple values like complete html page;
 //Starting point;
-echo<<<my
+echo <<<my
 <h1>Hello World</h1>
 <br>
 <h2>Hi Ameer ali</h2>
@@ -21,19 +54,8 @@ echo<<<my
 my;
 //Ending point
 
-//Variables
-$num=3.4;
-echo "Is num : ",$num;
-
-//const variable first name is const variable name and after values;
-define("a","Hello const variable");
-
-
-//Dump use for value check any argument;
-var_dump($num);
-
 
 //plus functions
-echo 2+2;
+echo 2 + 2;
 
 ?>
