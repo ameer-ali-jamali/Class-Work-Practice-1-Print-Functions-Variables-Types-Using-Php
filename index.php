c49d8cd5095240bbef8cdda457913253b528413b
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-11-09 19:02:24 +0500

    now code will be understand easily, mentioned.-Exp

--- index.php
+++ index.php
@@ -1,7 +1,55 @@
+PHP Overview
+PHP is a server-side scripting language, which is used to manage dynamic web pages, databases and build websites with features like session tracking and e-commerce. On a day of 1995, Rasmus Lerdorf unleashed the first version of “Hypertext Preprocessor” also known as the PHP language. It is also integrated with several popular databases like MySQL, PostgreSQL, Microsoft SQL Server, Oracle etc.
+
+
+
+Uses of PHP
+PHP can perform several system functions like opening files, CRUD operations on data stores, general-purpose scripting, etc. Besides system operations, there are also other uses like
+
+Handling Forms: PHP can handle form operations. It can gather data, save data to a file and send data through emails.
+Database Operations: PHP can also create, read, update and delete elements in your database.
+Encryption: It can perform advanced encryption and encrypt data for you.
+Dynamic Page Content: It can generate dynamic page content.
+
+
+Basic Syntax PHP
+A PHP script can be written anywhere inside the HTML document. A PHP script starts with tag and ends with We can write our logic inside tag and it will be executed accordingly.
+
+<?php
+// PHP code goes here
+?>
+
+
+Hello World
+A basic PHP Hello World program looks something like this. We will use a built-in PHP function “echo” to output the text “Hello World!” on our webpage.
+
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h1>My first PHP page</h1>
+    <?php
+    echo "Hello World!";
+    ?>
+</body>
+
+</html>
+
+PHP Comments
+A comment is a part of the coding file that the programmer does not want to execute, rather the programmer uses it to either explain a block of code or to avoid the execution of a specific part of code while testing.
+
+
+
+PHP supports several ways of commenting:
+Single Line Comments
+
 <?php
 // This is a single-line comment
 # This is also a single-line comment
 ?>
+
+
+Multiple-Line Comments
 
 <?php
 /*
@@ -9,53 +57,4 @@ This is a
 multiple line
 Comment.
 */
-?>
-
-<?php
-// PHP code goes here
-?>
-
-<?php
-
-$txt = "Hello world!";  # Type String
-$x = 5;                        # Type int
-$y = 10.5;         # Type Float
-
-//Variable
-$num = 3.4;
-echo "Is num : ", $num;
-
-//const variable first name is const variable name and after values;
-define("a", "Hello const variable");
-
-//....
-
-//Dump use for value check any argument;
-var_dump($num);
-
-
-// Print Functions;
-
-//Difference in echo an print;
-
-//Echo Get Multiple Values;
-echo "Hello world", "Hi Ameer Ali";
-
-//Print only get on value;
-print("hello");
-
-// Php buildin Function for multiple values like complete html page;
-//Starting point;
-echo <<<my
-<h1>Hello World</h1>
-<br>
-<h2>Hi Ameer ali</h2>
-
-my;
-//Ending point
-
-
-//plus functions
-echo 2 + 2;
-
 ?>
